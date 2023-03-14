c00ef3b4f4b1f1943e846feb7f9d1a8a65969dee
mikkopaa <mikkopaa@gmail.com>
2023-03-14 15:19:24 +0200

    Added login

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express');
 const connectDB = require('../config/db');
 const cors = require('cors');
 
 const images = require('./routes/api/images');
 const users = require('./routes/api/users')
+const loginRouter = require('./controllers/login')
 
 const app = express();
 
@@ -15,8 +17,9 @@ app.use(express.static('build'))
 
 app.get('/', (req, res) => res.send('hello...'));
 
-app.use('/api/images', images);
+app.use('/api/images', images)
 app.use('/api/users', users)
+app.use('/api/login', loginRouter)
 
 const port = process.env.PORT || 8082;
 
