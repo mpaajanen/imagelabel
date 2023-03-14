84f626484a849f17c5e0385f4ea0393800d66301
mikkopaa <mikko.paajanen@metropolia.fi>
2023-03-14 09:49:24 +0200

    Started adding users

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require('../config/db');
 const cors = require('cors');
 
 const images = require('./routes/api/images');
+const users = require('./routes/api/users')
 
 const app = express();
 
@@ -12,9 +13,10 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 app.use(express.static('build'))
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send('hello...'));
 
 app.use('/api/images', images);
+app.use('/api/users', users)
 
 const port = process.env.PORT || 8082;
 
