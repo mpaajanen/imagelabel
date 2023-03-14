0e9a65dd19ded60182faadf38e29fe2024574fc6
mikkopaa <mikkopaa@gmail.com>
2023-03-14 10:16:43 +0200

    Added getting all users

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../../models/User')
 
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, password } = request.body
 
